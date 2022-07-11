fe626186011fec279aec81f273e234e7b65c5e6f
mikeyy <adhikari.shrish1@gmail.com>
2022-07-11 15:46:10 +0545

    changes

--- app/app/settings.py
+++ app/app/settings.py
@@ -160,7 +160,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATIC_ROOT = '/static/'
-# print("STATIC ROOT IS", STATIC_ROOT)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
